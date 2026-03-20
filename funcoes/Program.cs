3810955477658634b59d1a1d03673f655257e78b
Fernando-星野 <102686701+Timerunyu@users.noreply.github.com>
2026-03-20 00:13:59 -0300

    SomaMedia
    Programa que calcula os valores digitados , faz a media dos valores , informa o menor e o maior numero digitado e para ao digitar um numero negativo

---
+++ funcoes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program{
+    public static void Main(){
+        int contador = 0;
+        int soma = 0;
+        int menorValor = int.MaxValue;
+        int maiorValor = int.MinValue;
+
+        while (true){
+            Console.Write("Informe um valor (número negativo para parar): ");
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor < 0)
+            {
+                break;
+            }
+
+            contador++;
+            soma += valor;
+
+            if (valor < menorValor)
+            {
+                menorValor = valor;
+            }
+
+            if (valor > maiorValor)
+            {
+                maiorValor = valor;
+            }
+        }
+
+        if (contador > 0)
+        {
+            double media = (double)soma / contador;
+
+            Console.WriteLine("n1 Contador de valores válidos: {contador}");
+            Console.WriteLine("2 Soma dos valores: " + soma);
+            Console.WriteLine("3 Média dos valores: "+ media);
+            Console.WriteLine("4 Menor valor informado: " + menorValor);
+            Console.WriteLine("5 Maior valor informado: "+ maiorValor);
+        }
+        else
+        {
+            Console.WriteLine("Nenhum valor válido foi informado.");
+        }
+    }
+}
