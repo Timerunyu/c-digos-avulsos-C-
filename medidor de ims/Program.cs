000da02402de3762bf3663d5449df18dfc7ace39
Fernando-星野 <102686701+Timerunyu@users.noreply.github.com>
2026-03-19 23:59:28 -0300

    código do medidor de ims

---
+++ medidor de ims/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main(){
+	double p,a,imc;
+		Console.WriteLine("digite seu peso");
+		p=double.Parse(Console.ReadLine());
+		Console.WriteLine("digite sua altura");
+		a=double.Parse(Console.ReadLine());
+		imc=p/Math.Pow(a,2);
+		if(imc<18){
+		Console.WriteLine("Desnutrido");
+		}
+		if((imc>=18)&&(imc<=20)){
+		Console.WriteLine("Baixo peso");	
+		}
+		if((imc>20)&&(imc<=24)){
+		Console.WriteLine("normal");
+		}
+		if((imc>24)&&(imc<=27)){
+		Console.WriteLine("sobre peso");
+		}
+		if(imc>27){
+		Console.WriteLine("obeso");
+		}
+	}
+}
