80f34a44edd52f207fa25a7b57c7e67459a98dd9
Fernando-星野 <102686701+Timerunyu@users.noreply.github.com>
2026-03-20 00:06:15 -0300

    um programa que calcula coordenadas

---
+++ cordenadas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main(){
+		double p,x1,x2,y1,y2;
+		double d;
+		Console.WriteLine("escreva as cordenadas ponto 1 ");
+		x1=int.Parse(Console.ReadLine());
+		y1=int.Parse(Console.ReadLine());
+		Console.WriteLine("digite as cordenadas do ponto 2");
+		x2=int.Parse(Console.ReadLine());
+		y2=int.Parse(Console.ReadLine());
+		d=Math.Sqrt(Math.Pow(x2-x1,2 + Math.Pow(y2-y1,2)));
+		Console.WriteLine("o resultado e " + d);
+		
+	}
+}
